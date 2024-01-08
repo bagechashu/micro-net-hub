633d293a05c6f1f98a52e7a9acfbb9a221a1293f
lixiaoxue <lixiaoxue@example.com>
2024-01-08 17:27:54 +0400

    update: fe: grouptype update comment

--- frontend/src/views/personnel/group/index.vue
+++ frontend/src/views/personnel/group/index.vue
@@ -3,13 +3,13 @@
     <el-card class="container-card" shadow="always">
       <el-form size="mini" :inline="true" :model="params" class="demo-form-inline">
         <el-form-item label="名称">
-          <el-input style="width: 100px;" v-model.trim="params.groupName" clearable placeholder="名称" @keyup.enter.native="search" @clear="search" />
+          <el-input v-model.trim="params.groupName" style="width: 100px;" clearable placeholder="名称" @keyup.enter.native="search" @clear="search" />
         </el-form-item>
         <el-form-item label="描述">
-          <el-input style="width: 100px;" v-model.trim="params.remark" clearable placeholder="描述" @keyup.enter.native="search" @clear="search" />
+          <el-input v-model.trim="params.remark" style="width: 100px;" clearable placeholder="描述" @keyup.enter.native="search" @clear="search" />
         </el-form-item>
-          <el-form-item label="同步状态">
-          <el-select style="width: 110px;" v-model.trim="params.syncState" clearable placeholder="同步状态" @change="search" @clear="search">
+        <el-form-item label="同步状态">
+          <el-select v-model.trim="params.syncState" style="width: 110px;" clearable placeholder="同步状态" @change="search" @clear="search">
             <el-option label="已同步" value="1" />
             <el-option label="未同步" value="2" />
           </el-select>
@@ -27,7 +27,7 @@
           <el-button :disabled="multipleSelection.length === 0" :loading="loading" icon="el-icon-delete" type="danger" @click="batchDelete">批量删除</el-button>
         </el-form-item>
         <el-form-item>
-          <el-button  :disabled="multipleSelection.length === 0" :loading="loading" icon="el-icon-upload2" type="success" @click="batchSync">批量同步</el-button>
+          <el-button :disabled="multipleSelection.length === 0" :loading="loading" icon="el-icon-upload2" type="success" @click="batchSync">批量同步</el-button>
         </el-form-item>
         <br>
         <el-form-item>
@@ -80,9 +80,9 @@
             <el-input v-model.trim="dialogFormData.groupName" placeholder="名称(拼音)" />
           </el-form-item>
           <el-form-item label="分组类型" prop="groupType">
-            <el-select v-model.trim="dialogFormData.groupType" placeholder="建议仅第一层为ou，如果不确定，就用cn" style="width:100%">
-              <el-option label="cn[分组]" value="cn" />
-              <el-option label="ou[组织]" value="ou" />
+            <el-select v-model.trim="dialogFormData.groupType" placeholder="协定 [ou] 只维护 group 相关的 [cn]; [cn] 下维护用户. 用户'所属部门'有体现." style="width:100%">
+              <el-option label="ou [organizationalUnit]" value="ou" />
+              <el-option label="cn [groupOfUniqueNames]" value="cn" />
             </el-select>
           </el-form-item>
           <el-form-item label="上级分组" prop="parentId">
@@ -123,24 +123,24 @@
 </template>
 
 <script>
-import Treeselect from '@riophae/vue-treeselect'
-import '@riophae/vue-treeselect/dist/vue-treeselect.css'
-import { getGroupTree,  groupAdd, groupUpdate, groupDel, syncDingTalkDeptsApi, syncWeComDeptsApi, syncFeiShuDeptsApi, syncOpenLdapDeptsApi, syncSqlGroups } from '@/api/personnel/group'
-import { Message } from 'element-ui'
+import Treeselect from "@riophae/vue-treeselect";
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
+import { getGroupTree, groupAdd, groupUpdate, groupDel, syncDingTalkDeptsApi, syncWeComDeptsApi, syncFeiShuDeptsApi, syncOpenLdapDeptsApi, syncSqlGroups } from "@/api/personnel/group";
+import { Message } from "element-ui";
 
 export default {
-  name: 'Group',
+  name: "Group",
   components: {
     Treeselect
   },
   filters: {
     methodTagFilter(val) {
-      if (val === 'GET') {
-        return ''
-      } else if (val === 'POST') {
-        return 'success'
+      if (val === "GET") {
+        return "";
+      } else if (val === "POST") {
+        return "success";
       } else {
-        return 'info'
+        return "info";
       }
     }
   },
@@ -165,40 +165,40 @@ export default {
       updateLoading: false, // 新增
       // dialog对话框
       submitLoading: false,
-      dialogFormTitle: '',
-      dialogType: '',
+      dialogFormTitle: "",
+      dialogType: "",
       dialogFormVisible: false,
       dialogFormData: {
-        ID: '',
-        groupName: '',
+        ID: "",
+        groupName: "",
         parentId: 0,
-        syncState:1,
-        groupType: '',
-        remark: ''
+        syncState: 1,
+        groupType: "",
+        remark: ""
       },
       dialogFormRules: {
 
         groupName: [
-          { required: true, message: '请输入所属类别', trigger: 'blur' },
-          { min: 1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur' }
+          { required: true, message: "请输入所属类别", trigger: "blur" },
+          { min: 1, max: 50, message: "长度在 1 到 50 个字符", trigger: "blur" }
         ],
         groupType: [
-          { required: true, message: '请输入分组类型', trigger: 'blur' },
-          { min: 1, max: 50, message: 'ou、cn或者其他', trigger: 'blur' }
+          { required: true, message: "请输入分组类型", trigger: "blur" },
+          { min: 1, max: 50, message: "ou、cn或者其他", trigger: "blur" }
         ],
         parentId: [
-          { required: true, message: '请选择父级', trigger: 'blur' },
+          { required: true, message: "请选择父级", trigger: "blur" },
           { validator: (rule, value, callBack) => {
             if (value >= 0) {
-              callBack()
+              callBack();
             } else {
-              callBack('请选择有效的部门')
+              callBack("请选择有效的部门");
             }
           } }
         ],
         remark: [
-          { required: false, message: '说明', trigger: 'blur' },
-          { min: 0, max: 100, message: '长度在 0 到 100 个字符', trigger: 'blur' }
+          { required: false, message: "说明", trigger: "blur" },
+          { min: 0, max: 100, message: "长度在 0 到 100 个字符", trigger: "blur" }
         ]
       },
 
@@ -206,15 +206,15 @@ export default {
       popoverVisible: false,
       // 表格多选
       multipleSelection: [],
-      dialogTransfer: '', // 穿梭框头部
+      dialogTransfer: "", // 穿梭框头部
       dialogTransferVisible: false,
 
       transParams: {
-        groupId: '',
-        nickname: ''
+        groupId: "",
+        nickname: ""
       },
       renderFunc(h, option) {
-        return <span>{option.key} - {option.label}</span>
+        return <span>{option.key} - {option.label}</span>;
       },
       userArrInfo: [], // 初始人员列表数据
       data: [], // 转化后人员列表数据
@@ -223,232 +223,232 @@ export default {
       ui: {
         submitLoading: false
       },
-      statusTrans: ''
-    }
+      statusTrans: ""
+    };
   },
   created() {
-    this.getTableData()
+    this.getTableData();
   },
   methods: {
     // // 查询
     search() {
       // 初始化表格数据
-      this.infoTableData = JSON.parse(JSON.stringify(this.tableData))
-      this.infoTableData = this.deal(this.infoTableData, node => node.groupName.includes(this.params.groupName) || node.remark.includes(this.params.remark)  || node.syncState.toString().includes(this.params.syncState))
+      this.infoTableData = JSON.parse(JSON.stringify(this.tableData));
+      this.infoTableData = this.deal(this.infoTableData, node => node.groupName.includes(this.params.groupName) || node.remark.includes(this.params.remark) || node.syncState.toString().includes(this.params.syncState));
     },
     resetData() {
-      this.infoTableData = JSON.parse(JSON.stringify(this.tableData))
+      this.infoTableData = JSON.parse(JSON.stringify(this.tableData));
     },
     // 页面数据过滤
     deal(nodes, predicate) {
       // 如果已经没有节点了，结束递归
       if (!(nodes && nodes.length)) {
-        return []
+        return [];
       }
-      const newChildren = []
+      const newChildren = [];
       for (const node of nodes) {
         if (predicate(node)) {
           // 如果节点符合条件，直接加入新的节点集
-          newChildren.push(node)
-          node.children = this.deal(node.children, predicate)
+          newChildren.push(node);
+          node.children = this.deal(node.children, predicate);
         } else {
           // 如果当前节点不符合条件，递归过滤子节点，
           // 把符合条件的子节点提升上来，并入新节点集
-          newChildren.push(...this.deal(node.children, predicate))
+          newChildren.push(...this.deal(node.children, predicate));
         }
       }
-      return newChildren
+      return newChildren;
     },
     // 获取表格数据
     async getTableData() {
-      this.loading = true
+      this.loading = true;
       try {
-        const { data } = await getGroupTree(this.params)
-        this.tableData = data
-        this.infoTableData = JSON.parse(JSON.stringify(data))
-        this.treeselectData = [{ ID: 0, groupName: '顶级类目', children: data }]
+        const { data } = await getGroupTree(this.params);
+        this.tableData = data;
+        this.infoTableData = JSON.parse(JSON.stringify(data));
+        this.treeselectData = [{ ID: 0, groupName: "顶级类目", children: data }];
       } finally {
-        this.loading = false
+        this.loading = false;
       }
     },
 
     // 新增
     create() {
-      this.dialogFormTitle = '新增分组'
-      this.updateLoading = true // 新增的展示
-      this.dialogType = 'create'
+      this.dialogFormTitle = "新增分组";
+      this.updateLoading = true; // 新增的展示
+      this.dialogType = "create";
     },
     // 修改
     update(row) {
-      this.dialogFormData.ID = row.ID
-      this.dialogFormData.groupName = row.groupName
-      this.dialogFormData.remark = row.remark
-      this.dialogFormTitle = '修改分组'
-      this.dialogType = 'update'
-      this.dialogFormVisible = true
+      this.dialogFormData.ID = row.ID;
+      this.dialogFormData.groupName = row.groupName;
+      this.dialogFormData.remark = row.remark;
+      this.dialogFormTitle = "修改分组";
+      this.dialogType = "update";
+      this.dialogFormVisible = true;
     },
     // 穿梭框
     addUp(row) {
-      this.dialogTransfer = '用户管理'
-      this.dialogTransferVisible = true
-      this.transParams.groupId = row.ID
-      this.transParams.nickname = row.remark
-      this.$router.push({ path: '/userList', query: row })
+      this.dialogTransfer = "用户管理";
+      this.dialogTransferVisible = true;
+      this.transParams.groupId = row.ID;
+      this.transParams.nickname = row.remark;
+      this.$router.push({ path: "/userList", query: row });
     },
 
     // 判断结果
-    judgeResult(res){
-      if (res.code==0){
-          Message({
-            showClose: true,
-            message: "操作成功",
-            type: 'success'
-          })
-        }
+    judgeResult(res) {
+      if (res.code === 0) {
+        Message({
+          showClose: true,
+          message: "操作成功",
+          type: "success"
+        });
+      }
     },
 
     // 提交表单
     submitForm() {
-      this.$refs['dialogForm'].validate(async valid => {
+      this.$refs["dialogForm"].validate(async valid => {
         if (valid) {
-          this.submitLoading = true
+          this.submitLoading = true;
           try {
-            if (this.dialogType === 'create') {
-              await groupAdd(this.dialogFormData).then(res =>{
-                this.judgeResult(res)
-              })
+            if (this.dialogType === "create") {
+              await groupAdd(this.dialogFormData).then(res => {
+                this.judgeResult(res);
+              });
             } else {
-              await groupUpdate(this.dialogFormData).then(res =>{
-                this.judgeResult(res)
-              })
+              await groupUpdate(this.dialogFormData).then(res => {
+                this.judgeResult(res);
+              });
             }
           } finally {
-            this.submitLoading = false
+            this.submitLoading = false;
           }
-          this.resetForm()
-          this.getTableData()
+          this.resetForm();
+          this.getTableData();
         } else {
           Message({
             showClose: true,
-            message: '表单校验失败',
-            type: 'warn'
-          })
-          return false
+            message: "表单校验失败",
+            type: "warn"
+          });
+          return false;
         }
-      })
+      });
     },
 
     // 提交表单
     cancelForm() {
-      this.resetForm()
+      this.resetForm();
     },
 
     resetForm() {
-      this.dialogFormVisible = false
-      this.updateLoading = false
-      this.$refs['dialogForm'].resetFields()
+      this.dialogFormVisible = false;
+      this.updateLoading = false;
+      this.$refs["dialogForm"].resetFields();
       this.dialogFormData = {
 
-        groupName: '',
-        remark: ''
-      }
+        groupName: "",
+        remark: ""
+      };
     },
 
     // 批量删除
     batchDelete() {
-      this.$confirm('此操作将永久删除, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
+      this.$confirm("此操作将永久删除, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
       }).then(async res => {
-        this.loading = true
-        const groupIds = []
+        this.loading = true;
+        const groupIds = [];
         this.multipleSelection.forEach(x => {
-          groupIds.push(x.ID)
-        })
+          groupIds.push(x.ID);
+        });
         try {
           await groupDel({ groupIds: groupIds }).then(res => {
-            this.judgeResult(res)
-          })
+            this.judgeResult(res);
+          });
         } finally {
-          this.loading = false
+          this.loading = false;
         }
-        this.getTableData()
+        this.getTableData();
       }).catch(() => {
         Message({
           showClose: true,
-          type: 'info',
-          message: '已取消删除'
-        })
-      })
+          type: "info",
+          message: "已取消删除"
+        });
+      });
     },
     // 批量同步
     batchSync() {
-      this.$confirm('此操作批量同步数据到Ldap, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
+      this.$confirm("此操作批量同步数据到Ldap, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
       }).then(async res => {
-        this.loading = true
-        const groupIds = []
+        this.loading = true;
+        const groupIds = [];
         this.multipleSelection.forEach(x => {
-          groupIds.push(x.ID)
-        })
+          groupIds.push(x.ID);
+        });
         try {
           await syncSqlGroups({ groupIds: groupIds }).then(res => {
-            this.judgeResult(res)
-          })
+            this.judgeResult(res);
+          });
         } finally {
-          this.loading = false
+          this.loading = false;
         }
-        this.getTableData()
+        this.getTableData();
       }).catch(() => {
         Message({
           showClose: true,
-          type: 'info',
-          message: '已取消同步'
-        })
-      })
+          type: "info",
+          message: "已取消同步"
+        });
+      });
     },
 
     // 表格多选
     handleSelectionChange(val) {
-      this.multipleSelection = val
+      this.multipleSelection = val;
     },
 
     // 单个删除
     async singleDelete(Id) {
-      this.loading = true
+      this.loading = true;
       try {
-        await groupDel({ groupIds: [Id] }).then(res =>{
-          this.judgeResult(res)
-        })
+        await groupDel({ groupIds: [Id] }).then(res => {
+          this.judgeResult(res);
+        });
       } finally {
-        this.loading = false
+        this.loading = false;
       }
-      this.getTableData()
+      this.getTableData();
     },
     // 单个同步
     async singleSync(Id) {
-      this.loading = true
+      this.loading = true;
       try {
-        await syncSqlGroups({ groupIds: [Id] }).then(res =>{
-          this.judgeResult(res)
-        })
+        await syncSqlGroups({ groupIds: [Id] }).then(res => {
+          this.judgeResult(res);
+        });
       } finally {
-        this.loading = false
+        this.loading = false;
       }
-      this.getTableData()
+      this.getTableData();
     },
 
     // 分页
     handleSizeChange(val) {
-      this.params.pageSize = val
-      this.getTableData()
+      this.params.pageSize = val;
+      this.getTableData();
     },
     handleCurrentChange(val) {
-      this.params.pageNum = val
-      this.getTableData()
+      this.params.pageNum = val;
+      this.getTableData();
     },
     // treeselect
     normalizer(node) {
@@ -456,45 +456,45 @@ export default {
         id: node.ID,
         label: node.groupName,
         children: node.children
-      }
+      };
     },
     treeselectInput(value) {
-      this.treeselectValue = value
+      this.treeselectValue = value;
     },
     syncDingTalkDepts() {
-      this.loading = true
+      this.loading = true;
       syncDingTalkDeptsApi().then(res => {
-        this.judgeResult(res)
-        this.loading = false
-        this.getTableData()
-      })
+        this.judgeResult(res);
+        this.loading = false;
+        this.getTableData();
+      });
     },
     syncWeComDepts() {
-      this.loading = true
+      this.loading = true;
       syncWeComDeptsApi().then(res => {
-        this.judgeResult(res)
-        this.loading = false
-        this.getTableData()
-      })
+        this.judgeResult(res);
+        this.loading = false;
+        this.getTableData();
+      });
     },
     syncFeiShuDepts() {
-      this.loading = true
+      this.loading = true;
       syncFeiShuDeptsApi().then(res => {
-        this.judgeResult(res)
-        this.loading = false
-        this.getTableData()
-      })
+        this.judgeResult(res);
+        this.loading = false;
+        this.getTableData();
+      });
     },
     syncOpenLdapDepts() {
-      this.loading = true
+      this.loading = true;
       syncOpenLdapDeptsApi().then(res => {
-        this.judgeResult(res)
-        this.loading = false
-        this.getTableData()
-      })
+        this.judgeResult(res);
+        this.loading = false;
+        this.getTableData();
+      });
     }
   }
-}
+};
 </script>
 
 <style scoped>
