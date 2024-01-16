a49ae680bcb48b3021b252a64bf77d25f9be0839
lixiaoxue <lixiaoxue@example.com>
2024-01-16 22:21:46 +0400

    update: fe: fix messagebox of login info

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,16 +1,16 @@
-import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
-import store from '@/store'
+import axios from "axios";
+import { MessageBox, Message } from "element-ui";
+import store from "@/store";
 // import router from '@/router'
-import { getToken } from '@/utils/auth'
-import router from '@/router'
+import { getToken } from "@/utils/auth";
+import router from "@/router";
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? process.env.API_BASE_PATH : '/' // api 的 base_url
+  baseURL: process.env.NODE_ENV === "production" ? process.env.API_BASE_PATH : "/" // api 的 base_url
   // withCredentials: true, // send cookies when cross-domain requests
   // timeout: 5000 // request timeout
-})
+});
 
 // request interceptor
 service.interceptors.request.use(
@@ -20,16 +20,16 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['Authorization'] = 'Bearer ' + getToken()
+      config.headers["Authorization"] = "Bearer " + getToken();
       // config.headers['Content-Type'] = 'application/json'
     }
-    return config
+    return config;
   },
   error => {
     // do something with request error
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
 // response interceptor
 service.interceptors.response.use(
@@ -44,57 +44,57 @@ service.interceptors.response.use(
    * You can also judge the status by HTTP Status Code
    */
   response => {
-    const res = response.data
+    const res = response.data;
     if (res.code === 0 || res.code === 200) {
-      return res
+      return res;
     } else {
       Message({
         message: res.msg,
-        type: 'error'
-      })
-      return false
+        type: "error"
+      });
+      return false;
     }
   },
   error => {
     if (error.response.status === 401) {
-      if (error.response.data.message.indexOf('JWT认证失败') !== -1) {
+      if (error.response.data.message.indexOf("JWT认证失败") !== -1) {
         MessageBox.confirm(
-          '登录失败,用户名或密码错误,重新登录或继续停留在当前页？',
-          '登录状态已失效',
+          "登录状态过期, 或登录失败. 请重新登录.",
+          "登录状态已失效",
           {
-            confirmButtonText: '重新登录',
-            cancelButtonText: '继续停留',
-            type: 'warning'
+            confirmButtonText: "强行刷新",
+            cancelButtonText: "重新登录",
+            type: "warning"
           }
         ).then(() => {
           // store.dispatch('user/resetToken').then(() => {
           //   location.reload()
           // })
-          store.dispatch('user/logout').then(() => {
-            location.reload()
-          })
-        })
+          store.dispatch("user/logout").then(() => {
+            location.reload();
+          });
+        });
       } else {
         Message({
           showClose: true,
           message: error.response.data.message,
-          type: 'error',
+          type: "error",
           duration: 5 * 1000
-        })
-        return Promise.reject(error)
+        });
+        return Promise.reject(error);
       }
     } else if (error.response.status === 403) {
-      router.push({ path: '/401' })
+      router.push({ path: "/401" });
     } else {
       Message({
         showClose: true,
         message: error.response.data.message || error.message,
-        type: 'error',
+        type: "error",
         duration: 5 * 1000
-      })
-      return Promise.reject(error)
+      });
+      return Promise.reject(error);
     }
   }
-)
+);
 
-export default service
+export default service;
