d7b574ed3e38ef7e0a015099c7a2602db5babb82
lixiaoxue <lixiaoxue@example.com>
2024-05-01 05:39:00 +0400

    tmp: refactoring menu handler

--- backend/internal/module/account/menu/menu_handler.go
+++ backend/internal/module/account/menu/menu_handler.go
@@ -14,178 +14,217 @@ import (
 
 // GetTree 菜单树
 func GetTree(c *gin.Context) {
-	req := new(accountModel.MenuGetTreeReq)
-	helper.HandleRequest(c, req, func(c *gin.Context, req interface{}) (data interface{}, rspError interface{}) {
-		_, ok := req.(*accountModel.MenuGetTreeReq)
-		if !ok {
-			return nil, helper.ReqAssertErr
-		}
-		_ = c
-		var menus = accountModel.NewMenus()
-		err := menus.List()
-		if err != nil {
-			return nil, helper.NewMySqlError(fmt.Errorf("获取资源列表失败: " + err.Error()))
-		}
+	var menus = accountModel.NewMenus()
+	err := menus.List()
+	if err != nil {
+		helper.ErrV2(c, helper.NewMySqlError(fmt.Errorf("获取资源列表失败: %s", err.Error())))
+		return
+	}
 
-		tree := accountModel.GenMenuTree(0, menus)
+	tree := accountModel.GenMenuTree(0, menus)
+	helper.Success(c, tree)
+}
 
-		return tree, nil
-	})
+// MenuGetAccessTreeReq 用户菜单树request struct
+type MenuGetAccessTreeReq struct {
+	ID uint `json:"id" form:"id"`
 }
 
 // GetUserMenuTreeByUserId 获取用户菜单树
 func GetAccessTree(c *gin.Context) {
-	req := new(accountModel.MenuGetAccessTreeReq)
-	helper.HandleRequest(c, req, func(c *gin.Context, req interface{}) (data interface{}, rspError interface{}) {
-		r, ok := req.(*accountModel.MenuGetAccessTreeReq)
-		if !ok {
-			return nil, helper.ReqAssertErr
-		}
-		_ = c
-		// 校验
-		filter := tools.H{"id": r.ID}
-		var u accountModel.User
-		if !u.Exist(filter) {
-			return nil, helper.NewValidatorError(fmt.Errorf("该用户不存在"))
-		}
-		user := new(accountModel.User)
-		err := user.Find(filter)
-		if err != nil {
-			return nil, helper.NewMySqlError(fmt.Errorf("在MySQL查询用户失败: " + err.Error()))
-		}
-		var roleIds []uint
-		for _, role := range user.Roles {
-			roleIds = append(roleIds, role.ID)
-		}
-		var menus = accountModel.NewMenus()
-		err = menus.ListUserMenus(roleIds)
-		if err != nil {
-			return nil, helper.NewMySqlError(fmt.Errorf("获取资源列表失败: " + err.Error()))
-		}
+	var req MenuGetAccessTreeReq
+	err := helper.BindAndValidateRequest(c, &req)
+	if err != nil {
+		return
+	}
 
-		tree := accountModel.GenMenuTree(0, menus)
+	// 校验
+	filter := tools.H{"id": req.ID}
+	var u accountModel.User
+	if !u.Exist(filter) {
+		helper.ErrV2(c, helper.NewValidatorError(fmt.Errorf("该用户不存在")))
+		return
+	}
+	user := new(accountModel.User)
+	err = user.Find(filter)
+	if err != nil {
+		helper.ErrV2(c, helper.NewMySqlError(fmt.Errorf("查询用户失败: %s", err.Error())))
+		return
+	}
+	var roleIds []uint
+	for _, role := range user.Roles {
+		roleIds = append(roleIds, role.ID)
+	}
+	var menus = accountModel.NewMenus()
+	err = menus.ListUserMenus(roleIds)
+	if err != nil {
+		helper.ErrV2(c, helper.NewMySqlError(fmt.Errorf("获取资源列表失败: %s", err.Error())))
+		return
+	}
 
-		return tree, nil
-	})
+	tree := accountModel.GenMenuTree(0, menus)
+
+	helper.Success(c, tree)
+}
+
+// MenuAddReq 添加资源结构体
+type MenuAddReq struct {
+	Name       string `json:"name" validate:"required,min=1,max=50"`
+	Title      string `json:"title" validate:"required,min=1,max=50"`
+	Icon       string `json:"icon" validate:"min=0,max=50"`
+	Path       string `json:"path" validate:"required,min=1,max=100"`
+	Redirect   string `json:"redirect" validate:"min=0,max=100"`
+	Component  string `json:"component" validate:"required,min=1,max=100"`
+	Sort       uint   `json:"sort" validate:"gte=1,lte=999"`
+	Status     uint   `json:"status" validate:"oneof=1 2"`
+	Hidden     uint   `json:"hidden" validate:"oneof=1 2"`
+	NoCache    uint   `json:"noCache" validate:"oneof=1 2"`
+	AlwaysShow uint   `json:"alwaysShow" validate:"oneof=1 2"`
+	Breadcrumb uint   `json:"breadcrumb" validate:"oneof=1 2"`
+	ActiveMenu string `json:"activeMenu" validate:"min=0,max=100"`
+	ParentId   uint   `json:"parentId"`
 }
 
 // Add 新建
 func Add(c *gin.Context) {
-	req := new(accountModel.MenuAddReq)
-	helper.HandleRequest(c, req, func(c *gin.Context, req interface{}) (data interface{}, rspError interface{}) {
-		r, ok := req.(*accountModel.MenuAddReq)
-		if !ok {
-			return nil, helper.ReqAssertErr
-		}
-		_ = c
+	var req MenuAddReq
+	err := helper.BindAndValidateRequest(c, &req)
+	if err != nil {
+		return
+	}
 
-		// 获取当前用户
-		ctxUser, err := current.GetCurrentLoginUser(c)
-		if err != nil {
-			return nil, helper.NewMySqlError(fmt.Errorf("获取当前登陆用户信息失败"))
-		}
+	// 获取当前用户
+	ctxUser, err := current.GetCurrentLoginUser(c)
+	if err != nil {
+		helper.ErrV2(c, helper.NewMySqlError(fmt.Errorf("获取当前登陆用户信息失败: %s", err.Error())))
+		return
+	}
 
-		var menu = &accountModel.Menu{
-			Name:       r.Name,
-			Title:      r.Title,
-			Icon:       r.Icon,
-			Path:       r.Path,
-			Redirect:   r.Redirect,
-			Component:  r.Component,
-			Sort:       r.Sort,
-			Status:     r.Status,
-			Hidden:     r.Hidden,
-			NoCache:    r.NoCache,
-			AlwaysShow: r.AlwaysShow,
-			Breadcrumb: r.Breadcrumb,
-			ActiveMenu: r.ActiveMenu,
-			ParentId:   r.ParentId,
-			Creator:    ctxUser.Username,
-		}
-		if menu.Exist(tools.H{"name": r.Name}) {
-			return nil, helper.NewMySqlError(fmt.Errorf("菜单名称已存在"))
+	var menu = &accountModel.Menu{
+		Name:       req.Name,
+		Title:      req.Title,
+		Icon:       req.Icon,
+		Path:       req.Path,
+		Redirect:   req.Redirect,
+		Component:  req.Component,
+		Sort:       req.Sort,
+		Status:     req.Status,
+		Hidden:     req.Hidden,
+		NoCache:    req.NoCache,
+		AlwaysShow: req.AlwaysShow,
+		Breadcrumb: req.Breadcrumb,
+		ActiveMenu: req.ActiveMenu,
+		ParentId:   req.ParentId,
+		Creator:    ctxUser.Username,
+	}
+	if menu.Exist(tools.H{"name": req.Name}) {
+		helper.ErrV2(c, helper.NewMySqlError(fmt.Errorf("菜单名称已存在: %s", err.Error())))
+		return
+	}
+	err = menu.Add()
+	if err != nil {
+		helper.ErrV2(c, helper.NewMySqlError(fmt.Errorf("创建记录失败: %s", err.Error())))
+		return
+	}
 
-		}
-		err = menu.Add()
-		if err != nil {
-			return nil, helper.NewMySqlError(fmt.Errorf("创建记录失败: %s", err.Error()))
-		}
+	helper.Success(c, nil)
+}
 
-		return nil, nil
-	})
+// MenuUpdateReq 更新资源结构体
+type MenuUpdateReq struct {
+	ID         uint   `json:"id" validate:"required"`
+	Name       string `json:"name" validate:"required,min=1,max=50"`
+	Title      string `json:"title" validate:"required,min=1,max=50"`
+	Icon       string `json:"icon" validate:"min=0,max=50"`
+	Path       string `json:"path" validate:"required,min=1,max=100"`
+	Redirect   string `json:"redirect" validate:"min=0,max=100"`
+	Component  string `json:"component" validate:"min=0,max=100"`
+	Sort       uint   `json:"sort" validate:"gte=1,lte=999"`
+	Status     uint   `json:"status" validate:"oneof=1 2"`
+	Hidden     uint   `json:"hidden" validate:"oneof=1 2"`
+	NoCache    uint   `json:"noCache" validate:"oneof=1 2"`
+	AlwaysShow uint   `json:"alwaysShow" validate:"oneof=1 2"`
+	Breadcrumb uint   `json:"breadcrumb" validate:"oneof=1 2"`
+	ActiveMenu string `json:"activeMenu" validate:"min=0,max=100"`
+	ParentId   uint   `json:"parentId" validate:"min=0,max=1000"`
 }
 
 // Update 更新记录
 func Update(c *gin.Context) {
-	req := new(accountModel.MenuUpdateReq)
-	helper.HandleRequest(c, req, func(c *gin.Context, req interface{}) (data interface{}, rspError interface{}) {
-		r, ok := req.(*accountModel.MenuUpdateReq)
-		if !ok {
-			return nil, helper.ReqAssertErr
-		}
-		_ = c
+	var req MenuUpdateReq
+	err := helper.BindAndValidateRequest(c, &req)
+	if err != nil {
+		return
+	}
 
-		oldMenu := new(accountModel.Menu)
-		filter := tools.H{"id": int(r.ID)}
-		if !oldMenu.Exist(filter) {
-			return nil, helper.NewMySqlError(fmt.Errorf("该ID对应的记录不存在"))
-		}
+	oldMenu := new(accountModel.Menu)
+	filter := tools.H{"id": int(req.ID)}
+	if !oldMenu.Exist(filter) {
+		helper.ErrV2(c, helper.NewMySqlError(fmt.Errorf("该ID对应的记录不存在: %s", err.Error())))
+		return
+	}
 
-		// 获取当前登陆用户
-		ctxUser, err := current.GetCurrentLoginUser(c)
-		if err != nil {
-			return nil, helper.NewMySqlError(fmt.Errorf("获取当前登陆用户失败"))
-		}
+	// 获取当前登陆用户
+	ctxUser, err := current.GetCurrentLoginUser(c)
+	if err != nil {
+		helper.ErrV2(c, helper.NewMySqlError(fmt.Errorf("获取当前登陆用户失败: %s", err.Error())))
+	}
 
-		newMenu := &accountModel.Menu{
-			Model:      oldMenu.Model,
-			Name:       r.Name,
-			Title:      r.Title,
-			Icon:       r.Icon,
-			Path:       r.Path,
-			Redirect:   r.Redirect,
-			Component:  r.Component,
-			Sort:       r.Sort,
-			Status:     r.Status,
-			Hidden:     r.Hidden,
-			NoCache:    r.NoCache,
-			AlwaysShow: r.AlwaysShow,
-			Breadcrumb: r.Breadcrumb,
-			ActiveMenu: r.ActiveMenu,
-			ParentId:   r.ParentId,
-			Creator:    ctxUser.Username,
-		}
-		err = newMenu.Update()
-		if err != nil {
-			return nil, helper.NewMySqlError(fmt.Errorf("更新记录失败: %s", err.Error()))
-		}
+	newMenu := &accountModel.Menu{
+		Model:      oldMenu.Model,
+		Name:       req.Name,
+		Title:      req.Title,
+		Icon:       req.Icon,
+		Path:       req.Path,
+		Redirect:   req.Redirect,
+		Component:  req.Component,
+		Sort:       req.Sort,
+		Status:     req.Status,
+		Hidden:     req.Hidden,
+		NoCache:    req.NoCache,
+		AlwaysShow: req.AlwaysShow,
+		Breadcrumb: req.Breadcrumb,
+		ActiveMenu: req.ActiveMenu,
+		ParentId:   req.ParentId,
+		Creator:    ctxUser.Username,
+	}
+	err = newMenu.Update()
+	if err != nil {
+		helper.ErrV2(c, helper.NewMySqlError(fmt.Errorf("更新记录失败: %s", err.Error())))
+		return
+	}
 
-		return nil, nil
-	})
+	helper.Success(c, nil)
+}
+
+// MenuDeleteReq 删除资源结构体
+type MenuDeleteReq struct {
+	MenuIds []uint `json:"menuIds" validate:"required"`
 }
 
 // Delete 删除记录
 func Delete(c *gin.Context) {
-	req := new(accountModel.MenuDeleteReq)
-	helper.HandleRequest(c, req, func(c *gin.Context, req interface{}) (data interface{}, rspError interface{}) {
-		r, ok := req.(*accountModel.MenuDeleteReq)
-		if !ok {
-			return nil, helper.ReqAssertErr
-		}
-		_ = c
+	var req MenuDeleteReq
 
-		for _, id := range r.MenuIds {
-			filter := tools.H{"id": int(id)}
-			var menu = new(accountModel.Menu)
-			if !menu.Exist(filter) {
-				return nil, helper.NewMySqlError(fmt.Errorf("menu ID: %d 对应的记录不存在", menu.ID))
-			}
-			// 删除接口
-			err := menu.Delete()
-			if err != nil {
-				return nil, helper.NewMySqlError(fmt.Errorf("%s删除接口失败: %s", menu.Name, err.Error()))
-			}
+	err := helper.BindAndValidateRequest(c, &req)
+	if err != nil {
+		return
+	}
+
+	for _, id := range req.MenuIds {
+		filter := tools.H{"id": int(id)}
+		var menu = new(accountModel.Menu)
+		if !menu.Exist(filter) {
+			helper.ErrV2(c, helper.NewMySqlError(fmt.Errorf("menu ID: %d 对应的记录不存在", menu.ID)))
+			return
 		}
-		return nil, nil
-	})
+		// 删除接口
+		err := menu.Delete()
+		if err != nil {
+			helper.ErrV2(c, helper.NewMySqlError(fmt.Errorf("%s删除接口失败: %s", menu.Name, err.Error())))
+			return
+		}
+	}
+
+	helper.Success(c, nil)
 }
