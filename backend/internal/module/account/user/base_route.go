09f0a92bc01383b5439a726f56e7abd5a2cea5ab
lixiaoxue <lixiaoxue@example.com>
2024-05-13 15:32:50 +0400

    update: add comment for decryptpwd api

--- backend/internal/module/account/user/base_route.go
+++ backend/internal/module/account/user/base_route.go
@@ -11,9 +11,10 @@ func InitBaseRoutes(r *gin.RouterGroup, authMiddleware *jwt.GinJWTMiddleware) gi
 	{
 		base.GET("/encryptpwd", EncryptPasswd) // 生成加密密码
 		// base.GET("/decryptpwd", DecryptPasswd) // 密码解密为明文
-		// 登录登出刷新token无需鉴权
-		base.POST("/login", authMiddleware.LoginHandler)
-		base.POST("/logout", authMiddleware.LogoutHandler)
+		// curl "http://127.0.0.1:9000/api/base/decryptpwd?passwd=$(echo 'D5TYG4U29/iqD+99lkXIxuwP0bHPLz5GvIIVhx9ZHobecad6JDgw2p5EFreRx3UWIwL9WJH1E32yzREdhkWlrZTgQ5GuKTItk34ZPtwnVLc+fgwSJ9OiYvaEzWOQXwMdWdo7sdpE89R4XktHPRXMULiPT3rTC8hT4pGG16RxqRo=' | sed 's/+/%2B/g; s/\//%2F/g; s/=/%3D/g')"
+
+		base.POST("/login", authMiddleware.LoginHandler)   // 登出刷新token无需鉴权
+		base.POST("/logout", authMiddleware.LogoutHandler) // 登出刷新token无需鉴权
 		base.POST("/refreshToken", authMiddleware.RefreshHandler)
 		base.POST("/sendcode", SendCode)   // 给用户邮箱发送验证码
 		base.POST("/changePwd", ChangePwd) // 修改用户密码
